6c2eb8dbcdab59ba706ca190d0b1f09ab9bb6289
Tamás Szelei <szelei.t@gmail.com>
2018-12-15 14:26:13 +0100

    Add debian bootstrap script

---
+++ bootstrap/debian.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+set -v
+
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+
+sudo apt install -y zsh tmux make build-essential libssl-dev zlib1g-dev libbz2-dev \
+libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
+xz-utils tk-dev libffi-dev liblzma-dev
+sudo apt install -y yarn --no-install-recommends
+
+pyenv install 3.6.4
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+
+chsh -s $(which zsh)
+
+pip install thefuck
+
+export NVM_DIR="$HOME/.nvm"
+[[ -s $NVM_DIR/nvm.sh ]] && . $NVM_DIR/nvm.sh  # This loads NVM
+
+nvm install node
