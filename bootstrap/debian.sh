e6e5ec2a18dfff1f4faea4ccfbb3a1525622b010
Tamás Szelei <szelei.t@gmail.com>
2019-01-15 17:15:18 +0100

    Add xclip and aliases

--- bootstrap/debian.sh
+++ bootstrap/debian.sh
@@ -14,7 +14,7 @@ sudo apt update
 
 sudo apt install -y zsh tmux make build-essential libssl-dev zlib1g-dev libbz2-dev \
 libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
-xz-utils tk-dev libffi-dev liblzma-dev ruby-full rofi xbindkeys
+xz-utils tk-dev libffi-dev liblzma-dev ruby-full rofi xbindkeys xclip
 sudo apt install -y yarn --no-install-recommends
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
