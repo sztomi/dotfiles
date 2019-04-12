0b2b9a8b3f05467a339a7f6bf6600a10ae24d5c0
Tamás Szelei <szelei.t@gmail.com>
2019-04-12 16:01:07 +0000

    freebsd: more packages

--- bootstrap/freebsd.sh
+++ bootstrap/freebsd.sh
@@ -1,6 +1,9 @@
 #! /bin/sh
 
-sudo pkg install zsh python3 git fzy neovim bash bzip2 sqlite
+sudo pkg install zsh python3 git fzy neovim bash bzip2 sqlite \
+  curl gcc gmake libstdc++_stldoc_4.2.2 ntp openjdk8 wget     \
+  autoconf v4l_compat sqlite3 gperf
+
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 echo "Running general Unix steps"
