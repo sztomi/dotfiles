0d6f23bb8d002d3f79b6108ddaa81704f0ae87e8
Tamás Szelei <szelei.t@gmail.com>
2019-04-12 14:19:28 +0200

    freebsd: install more packages

--- bootstrap/freebsd.sh
+++ bootstrap/freebsd.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-sudo pkg install zsh python3 git
+sudo pkg install zsh python3 git fzy neovim
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 echo "Running general Unix steps"
