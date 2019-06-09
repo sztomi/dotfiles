66593b1374d360b052ebe3090886d1d1b2d48615
Tamás Szelei <szelei.t@gmail.com>
2019-06-09 21:07:59 +0200

    debian: update packages list

--- bootstrap/debian.sh
+++ bootstrap/debian.sh
@@ -14,8 +14,10 @@ sudo apt update
 
 sudo apt install -y zsh tmux make build-essential libssl-dev zlib1g-dev libbz2-dev \
 libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
-xz-utils tk-dev libffi-dev liblzma-dev ruby-full rofi xbindkeys xclip
+xz-utils tk-dev libffi-dev liblzma-dev xbindkeys xclip gawk
 sudo apt install -y yarn --no-install-recommends
+
+sudo snap install lsd
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
