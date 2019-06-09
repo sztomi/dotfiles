2abf6f13a02d20c89170755fb71c27872f06643f
Tamás Szelei <szelei.t@gmail.com>
2019-06-09 21:52:04 +0200

    debian: don't install yarn

--- bootstrap/debian.sh
+++ bootstrap/debian.sh
@@ -1,13 +1,5 @@
 #! /bin/bash
 
-echo "Installing yarn"
-if ! [ -x $(command -v yarn) ]; then
-  curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-  echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-else
-  echo "yarn already installed"
-fi
-
 echo "Installing debian packages"
 
 sudo apt update
