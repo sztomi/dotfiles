f8aaefc1d68f1cc82c21c3b73d0fad026b0b5d55
Tamás Szelei <szelei.t@gmail.com>
2019-06-10 11:37:38 +0200

    debian: add libtinfo5

--- bootstrap/debian.sh
+++ bootstrap/debian.sh
@@ -6,7 +6,7 @@ sudo apt update
 
 sudo apt install -y zsh tmux make build-essential libssl-dev zlib1g-dev libbz2-dev \
 libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
-xz-utils tk-dev libffi-dev liblzma-dev xbindkeys xclip gawk
+xz-utils tk-dev libffi-dev liblzma-dev xbindkeys xclip gawk libtinfo5
 sudo apt install -y yarn --no-install-recommends
 
 sudo snap install lsd
