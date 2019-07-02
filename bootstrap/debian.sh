23f0a8722424a8f3490db5c18a1b007ad3b8bcb5
Tamás Szelei <szelei.t@gmail.com>
2019-07-02 16:42:55 +0200

    debian: add emoji font

--- bootstrap/debian.sh
+++ bootstrap/debian.sh
@@ -6,7 +6,8 @@ sudo apt update
 
 sudo apt install -y zsh tmux make build-essential libssl-dev zlib1g-dev libbz2-dev \
 libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
-xz-utils tk-dev libffi-dev liblzma-dev xbindkeys xclip gawk libtinfo5 gperf
+xz-utils tk-dev libffi-dev liblzma-dev xbindkeys xclip gawk libtinfo5 gperf \
+fonts-noto-color-emoji
 sudo apt install -y yarn --no-install-recommends
 
 sudo snap install lsd
