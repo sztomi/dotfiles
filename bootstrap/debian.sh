ae4b3a669e1214f893860401d0e4358ae6d17acf
Tamás Szelei <szelei.t@gmail.com>
2019-07-28 12:53:20 +0200

    debian: add devscripts

--- bootstrap/debian.sh
+++ bootstrap/debian.sh
@@ -7,7 +7,7 @@ sudo apt update
 sudo apt install -y zsh tmux make build-essential libssl-dev zlib1g-dev libbz2-dev \
 libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
 xz-utils tk-dev libffi-dev liblzma-dev xbindkeys xclip gawk libtinfo5 gperf \
-fonts-noto-color-emoji
+fonts-noto-color-emoji devscripts
 sudo apt install -y yarn --no-install-recommends
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
